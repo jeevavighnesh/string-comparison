463020d96bfa1e12373999d2912a6da45a56f795
VIGHNESH S <jeevavignesh@gmail.com>
2020-08-19 11:21:17 +0530

    Initial Commit
    A String Comparison Main class implemented

---
+++ StringComparison.java
@@ -0,0 +1,7 @@
+class StringComparison {
+    public static void main(String[] args) {
+        String str1 = args[0];
+        String str2 = args[1];
+        System.out.println(str1.equals(str2) ? "Both are same" : "Different!!!!");
+    }
+}
